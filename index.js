1fd1e7d4992a7db636e6b2c6e37d285c1df84534
Praveen Kumar <praveenshakya441@gmail.com>
2024-10-02 14:05:29 +0530

    add server

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const PORT = 3000;
+const app = express();
+const path= require("path");
+
+
+
+app.get("/", (req, res)=>{
+    res.sendFile(path.join(__dirname, "db.json"));
+})
+
+
+
+app.listen(PORT, ()=>{
+    console.log(`Server is running on port ${PORT}`)
+})
+
